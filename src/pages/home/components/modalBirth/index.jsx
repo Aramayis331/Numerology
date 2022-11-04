73587a0008b12b54ada75c39356bbab7327d4c33
Aramayis331 <aramayis.araqelyan.99@mail.ru>
2022-11-04 22:53:31 +0400

    Test calendar

--- src/pages/home/components/modalBirth/index.jsx
+++ src/pages/home/components/modalBirth/index.jsx
@@ -1,16 +1,20 @@
 import Modal from "components/modal";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
 import "./ModalBirth.scss";
 
 const ModalBirth = () => {
+  const [date, setDate] = useState("M8888M/DD/Y888Y9");
   const navigate = useNavigate();
-  const closeModal = () => {
-    navigate("/");
-  };
+
+  const handleChange = (newValue) => setDate(newValue);
+
+  const closeModal = () => navigate("/");
+
   return (
     <Modal onClose={closeModal}>
       <div className="modal__birth">
-        <input type="date" name="test" placeholder="MM/DD/YYYY" />
         <p className="modal__birth__title">Напишите свою дату</p>
         <p className="modal__birth__subTitle">
           рождения и получите первый анализ
@@ -22,11 +26,24 @@ const ModalBirth = () => {
           }}
         >
           <p className="modal__birth__form__title">Дата рождения</p>
+          <TextField
+            id="date"
+            type="date"
+            defaultValue="2017-05-24"
+            sx={{ width: 220 }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
           <input
             className="modal__birth__form__date"
             type="date"
             name="birth"
-            placeholder="MM/DD/YYYY"
+            date-format="yyyy-mm-dd"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+            value="2015-12-09"
           />
           <button className="modal__birth__form__button" type="submit">
             Отправить
