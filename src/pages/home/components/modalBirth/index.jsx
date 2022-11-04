40cb9fcb8ed429fd4d3e91c57ff99f10e1b55b6e
Aramayis331 <aramayis.araqelyan.99@mail.ru>
2022-11-04 22:06:25 +0400

    Added default value calendar

--- src/pages/home/components/modalBirth/index.jsx
+++ src/pages/home/components/modalBirth/index.jsx
@@ -10,7 +10,7 @@ const ModalBirth = () => {
   return (
     <Modal onClose={closeModal}>
       <div className="modal__birth">
-        <input type="date" name="test" />
+        <input type="date" name="test" defaultValue="MM/DD/YYYY" />
         <p className="modal__birth__title">Напишите свою дату</p>
         <p className="modal__birth__subTitle">
           рождения и получите первый анализ
@@ -26,6 +26,7 @@ const ModalBirth = () => {
             className="modal__birth__form__date"
             type="date"
             name="birth"
+            defaultValue="MM/DD/YYYY"
           />
           <button className="modal__birth__form__button" type="submit">
             Отправить
